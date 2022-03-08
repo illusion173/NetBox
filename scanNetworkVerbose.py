01a99656a362b5312e80cf4a536b7ead75250990
JeremiahLWebb <illusion173@hotmail.com>
2022-03-08 13:11:43 -0500

    Updated scanNetworkVerbose.py

--- scanNetworkVerbose.py
+++ scanNetworkVerbose.py
@@ -8,38 +8,36 @@ from colorama import Fore, Back, Style
 import subprocess
 import json
 
-print("****************************************************\n")
-print("Running ScanNetworkVerbose on Network")
-print("Are you sure you want to scan entire network?")
-print("Input 1 (MOST VERBOSE AND TIME CONSUMING): Prints verbose output, runs stealth syn scan, T5 timing, OS and version detection + full port range scan.")
-print("Input 2: Full TCP Port Scan detection")
-print("Input 3: Prints verbose output, runs stealth syn scan, T4 timing, OS and version detection + traceroute and scripts against target services.")
-print("Input 4: to exit the program.")
-userInput = input('Please input your selection: ')
-
-
 '''
-Essentially a boogie af switch statement.
+https://highon.coffee/blog/nmap-cheat-sheet/
 '''
-if userInput == '1':
 
-if userInput == '2':
+print("Scan Network Verbose Script running...\n")
+
+userInput = 'scanme.nmap.org'
+scanner = nmap.PortScanner()
+
+def saveFile(nm_json):
+    if os.path.exists("NetworkScanoutput.json"):
+        with open("NetworkScanoutput.json", "w", encoding="utf8") as outfile:
+            json.dump(nm_json, outfile, ensure_ascii=False, indent = 4)
+    if not os.path.exists("NetworkScanoutput.csv"):
+        file = open("NetworkScanoutput.json", "x")
+        with open("NetworkScanoutput.json", "w", encoding="utf8") as outfile:
+            json.dump(nm_json, outfile, ensure_ascii=False, indent = 4)
+
+def networkScanVerbose():
+    try:
+        scanData = scanner.scan(hosts = userInput, arguments = '-v -p 1-65535 -sV -O -sS -T4')
+        Output = scanData['scan']
+        saveFile(Output)
+    except KeyboardInterrupt:
+        sys.exit('\n^C\n')
+    except Exception as e: 
+        e = sys.exc_info()[1]
+        print(f'[{Fore.RED}!{Style.RESET_ALL}] Error: {Fore.RED}{e}{Style.RESET_ALL}')
+
+networkScanVerbose()
 
 
-if userInput == '3':
-
-
-if userInput == '4':
-
-
-
-proc = subprocess.check_output("ifconfig").decode('utf-8')
-print (proc)
-print("****************************************************\n")
-print("Popular Subnet CIDR Notations:\n")
-print("255.255.255.255 = /32")
-print("255.255.255.254 = /31")
-print("255.255.255.0 = /24")
-print("255.255.254.0 = /23")
-print("255.255.0.0 = /16\n")
-print("****************************************************\n")
+print("Done scanning...\n")
